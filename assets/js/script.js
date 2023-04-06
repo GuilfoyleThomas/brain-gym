84a303448350507631f9a6888bdaba1de55b8e3a
Thomas Guilfoyle <guilfoylethomas@outlook.com>
2023-04-06 02:48:53 +0000

    update nav links for 'help' and link script.js to all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-// Getting the "Help" link element by its ID
+// Getting the "Help" link
 const helpLink = document.getElementById("help-link");
 
 // Add a click event listener to the "Help" link
 helpLink.addEventListener("click", () => {
-    // Define an array of random words of encouragement
+    // Defines an array of random words of encouragement
     const encouragementWords = [
         "You can do it!",
         "Believe in yourself!",
@@ -19,9 +19,9 @@ helpLink.addEventListener("click", () => {
         "Success is not final, failure is not fatal: it is the courage to continue that counts. -Winston Churchill"
     ];
 
-    // Get a random word of encouragement from the array
+    // Getting random encouragment words from the array
     const randomEncouragement = encouragementWords[Math.floor(Math.random() * encouragementWords.length)];
 
-    // Show an alert with the random word of encouragement
+    // Show an alert with the random random encouragment words
     alert(randomEncouragement);
 });
